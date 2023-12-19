dc1538a822a77ad59846df5ea8f1178118cdd673
유영 <dbdud1999@naver.com>
2023-12-19 23:54:17 +0900

    :partying_face: 68936번 문제 풀이

--- src/PRO068936_쿼드압축후개수세기.java
+++ src/PRO068936_쿼드압축후개수세기.java
@@ -22,8 +22,6 @@ public class PRO068936_쿼드압축후개수세기 {
     private static void findZip(int split, int r, int c) {
         if (split == 1) {
             count[arr[r][c]]++;
-            // System.out.println(arr[r][c] + " 더하기");
-            // System.out.println("현재 총 개수 0: " + count[0] + ", 1: " + count[1]);
             return;
         }
         for (int i = 0; i < split; i += (split / 2)) {
@@ -44,8 +42,6 @@ public class PRO068936_쿼드압축후개수세기 {
 
         if (canZip) {
             count[arr[r][c]] -= 3;
-            // System.out.println(arr[r][c] + "를 3개 빼기");
         }
-        // System.out.println("현재 총 개수 0: " + count[0] + ", 1: " + count[1]);
     }
 }
