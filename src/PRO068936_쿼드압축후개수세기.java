6c9598cfea3a451c82468820a349d2aa63e71ba0
유영 <dbdud1999@naver.com>
2023-12-19 23:30:37 +0900

    :construction: 68936번 문제 풀이 진행중

---
+++ src/PRO068936_쿼드압축후개수세기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class PRO068936_쿼드압축후개수세기 {
+    private static int[][] arr;
+    public static void main(String[] args) {
+        arr = new int[][]{{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}};  // 테스트케이스
+        System.out.println(solution());
+    }
+
+    public static int[] solution() {
+        int[] answer = {};
+        return answer;
+    }
+}
