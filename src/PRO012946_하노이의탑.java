24edc2001ac79bf3e8ea6787a22c25895a720962
유영 <dbdud1999@naver.com>
2023-12-29 00:12:57 +0900

    :partying_face: 12946번 문제 풀이

--- src/PRO012946_하노이의탑.java
+++ src/PRO012946_하노이의탑.java
@@ -1,29 +1,26 @@
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class PRO012946_하노이의탑 {
-    private static Stack<Integer>[] stacks;
+    private static List<int[]> answer;
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solution(3)));
     }
 
     public static int[][] solution(int n) {
+        answer = new ArrayList<>();
         hanoi(n, 1, 2, 3);
-        return null;
+        return answer.toArray(new int[0][]);
     }
     
     private static void hanoi(int n, int start, int mid, int to) {
-        if(n == 1) {
-            //System.out.println("ㄲㅐㄱ");
-            System.out.println(start + "에서 " + to + "로 이동");
-            //System.out.println(start + "에서 " + to + "로 이동");
+        if(n == 1) {  // 하노이탑 한 개일 때
+            // System.out.println(start + " -> " + to);
+            answer.add(new int[]{start, to});
             return;
         }
-        System.out.println(start + "에서 " + to + "로 이동");
         hanoi(n - 1, start, to, mid);
-        //System.out.println(start + "에서 " + to + "로 이동");
-        System.out.println("꺅");
+        // System.out.println(start + " -> " + to);
+        answer.add(new int[]{start, to});
         hanoi(n - 1, mid, start, to);
     }
 }
